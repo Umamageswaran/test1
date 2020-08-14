344d30966930f684ff7c5f5f3603da3ed3f4b59c
umamageswaran <j.umamageswaram@gmail.com>
2020-08-14 19:25:08 +0530

    second commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello, Woeld from GIT!");
+		System.out.println("Changes after First Commit!");
+		System.out.println("Changes after Second Commit!");
 	}
 }
